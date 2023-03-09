542681de50caa1c75a625cd0b8a806b6a1031233
unknown <harriett.steed@gmail.com>
2023-03-08 23:02:46 -0700

    added else, else if, and if statements. added numbers to var

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -20,12 +20,9 @@ console.log(moreLetters);
 
 
 
-// special characters
+// special characters and numbers
 var special = ["!", "?", "@", "<", ">", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "/", "=", "+", "-", "`", "~", "[", "]"]; 
-
-
-
-
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
 
@@ -33,8 +30,6 @@ var special = ["!", "?", "@", "<", ">", "#", "$", "%", "^", "&", "*", "(", ")", 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
-  var passwordLength = 
 
   passwordText.value = password;
 
@@ -43,6 +38,35 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+function generatePassword() {
+  passwordLength = prompt ("Between 8 to 128 characters, how many would you like?");
+  console.log("Password length " + passwordLength);
+
+if (!passwordLength) {
+  alert("Required value");
+  }
+
+else {
+  Lower = confirm("Do you want to include lowercase letters?");
+  console.log("Lowercase " + Lower);
+
+  Upper = confirm("Do you want to include uppercase letters?");
+  console.log("Uppercase " + Upper);
+  
+  Special = confirm("Do you want to include special characters?");
+  console.log("Special character " + Special);
+
+  conNumber = confirm("Do you want to include numbers? ");
+  console.log("Number " + conNumber);
+  }
+
+
+else if (passwordLength <8 || passwordLength > 128) {
+  passwordLength = prompt("Choose between 8 and 128");
+  console.log("Password lenth " + passwordLength);
+  }
+}
 
 
 
