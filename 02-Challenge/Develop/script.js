d6d2386e77248d93d0022be7d1cfe241bce8d436
unknown <harriett.steed@gmail.com>
2023-03-09 23:41:08 -0700

    added the 4 needed conditional statements, added another var

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -7,6 +7,7 @@ var Upper;
 var Lower;
 var conNumber;
 var Special;
+var userChoice;
 
 // alphabet so password generates all letters 
 var Lower = ["a", "b", "c", "d", "e", "f","g", "h", "i", "j", "k", "l", "m", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -38,7 +39,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
+// start function
 function generatePassword() {
   passwordLength = prompt ("Between 8 to 128 characters, how many would you like?");
   console.log("Password length " + passwordLength);
@@ -46,6 +47,13 @@ function generatePassword() {
 if (!passwordLength) {
   alert("Required value");
   }
+
+  
+else if (passwordLength < 8 || passwordLength > 128) {
+  passwordLength = prompt("Choose between 8 and 128 characters");
+  console.log("Password length " + passwordLength);
+}
+
 
 else {
   Lower = confirm("Do you want to include lowercase letters?");
@@ -59,14 +67,82 @@ else {
 
   conNumber = confirm("Do you want to include numbers? ");
   console.log("Number " + conNumber);
-  }
+ } };
 
+// chose nothing
+if (!Lower && !Upper && !Special && !conNumber);
+userChoice = alert("Choose a criteria");
 
-else if (passwordLength <8 || passwordLength > 128) {
-  passwordLength = prompt("Choose between 8 and 128");
-  console.log("Password lenth " + passwordLength);
-  }
+// 1 true 
+else if (Lower) {
+  userChoice = lowerCase;
+  console.log(userChoice);
 }
+else if (Upper) {
+  userChoice = blankUpper.concat(upperCase);
+  console.log (userChoice);
+}
+else if (Special){
+  userChoice = special;
+  console.log(userChoice);
+}
+else if (conNumber) {
+  userChoice = numbers;
+  console.log(userChoice);
+};
+
+
+// 2 true options
+else if (Lower && Upper) {
+  userChoice = lowerCase.concat(upperCase);
+  console.log(userChoice);
+}
+
+else if (Lower && conNumber) {
+  userChoice = lowerCase.concat (numbers);
+  console.log(userChoice);
+}
+else if (Lower && Special) {
+  userChoice = lowerCase.concat(special);
+  console.log(userChoice);
+}
+else if (Upper && conNumber) {
+  userChoice = upperCase.concat(numbers);
+  console.log(userChoice);
+}
+else if (Upper && Special) {
+  userChoice =  upperCase.concat(special);
+  console.log(userChoice);
+}
+else if (conNumber && Special) {
+  userChoice = numbers.concat(special);
+  console.log(userChoice);
+}
+
+// 3 true
+else if (Lower && Upper && conNumber) {
+  userChoice = lowerCase.concat(upperCase, numbers);
+  console.log(userChoice);
+}
+else if (Lower && Upper && Special) {
+  userChoice = lowerCase.concat(upperCase, special);
+  console.log(userChoice);
+}
+else if (Lower && conNumber && Special) {
+  userChoice = lowerCase.concat(numbers, special);
+  console.log(userChoice);
+}
+else if (Upper && conNumber && Special) {
+  userChoice = upperCase.concat(numbers, special);
+  console.log(userChoice);
+}
+//  4 true
+else if (Lower && Upper && Special && conNumber) {
+  userChoice = lowerCase.concat(upperCase, special, numbers);
+  console.log(userChoice);
+}
+
+
 
 
 
